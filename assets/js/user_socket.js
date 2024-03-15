c9ad655a742604ccd2a26a300e1eb0d39051dbe9
Bernardo Martínez <bernardo.martinez@cabify.com>
2024-03-15 11:24:54 +0100

    initialize canvas

--- assets/js/user_socket.js
+++ assets/js/user_socket.js
@@ -2,6 +2,10 @@ const newBtn = document.querySelector( '#new' );
 
 newBtn.addEventListener('click', newNapkin);
 
+const paintCanvas = document.querySelector( '#canvas' );
+const context = paintCanvas.getContext( '2d' );
+context.lineCap = 'round';
+
 function newNapkin() {
   const uuid = crypto.randomUUID()
   location.href = "/" + uuid
