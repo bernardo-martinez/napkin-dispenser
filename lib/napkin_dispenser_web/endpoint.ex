da57166b9dd3e26e6b0239d0fe5f989081045f52
Bernardo Martínez <bernardo.martinez@cabify.com>
2024-03-19 14:41:03 +0100

    setup user socket

--- lib/napkin_dispenser_web/endpoint.ex
+++ lib/napkin_dispenser_web/endpoint.ex
@@ -11,6 +11,10 @@ defmodule NapkinDispenserWeb.Endpoint do
     same_site: "Lax"
   ]
 
+  socket "/socket", NapkinDispenserWeb.UserSocket,
+    websocket: [connect_info: [:peer_data]],
+    longpoll: false
+
   socket "/live", Phoenix.LiveView.Socket,
     websocket: [connect_info: [session: @session_options]],
     longpoll: [connect_info: [session: @session_options]]
